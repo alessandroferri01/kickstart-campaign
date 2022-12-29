25dff9353153b37abe32884801bd3c77a50b18d4
AleFerri01pd <ferri.alessandro2001@gmail.com>
2022-12-29 12:46:34 +0100

    add deploy logic and file with address of deployed contract

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,8 +1,8 @@
 // deploy code will go here
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { abi, evm } = require('./compile');
-provider = new HDWalletProvider(
+const compiledFactory = require("./build/CampaignFactory.json");
+const provider = new HDWalletProvider(
     'heavy zoo announce assist cluster blue tourist design place tray art cry', //Secret Recovery Phrase di Metamask
     'https://goerli.infura.io/v3/610553932f2747d189a138d911305bf3'  
 );
@@ -13,17 +13,16 @@ const deploy = async () => {
 
     console.log('Account' , accounts[0]);
 
-    const result = await new web3.eth.Contract(abi) //interface è ABI, oggetto JS del contratto
+    const result = await new web3.eth.Contract(compiledFactory.abi) //interface è ABI, oggetto JS del contratto
     .deploy({ 
-        data: evm.bytecode.object //codice binario per distribuire il contratto sulla rete ETH
+        data: compiledFactory.evm.bytecode.object //codice binario per distribuire il contratto sulla rete ETH
     })
     .send({
         from: accounts[0],
-        gas: '1000000'
+        gas: '1400000'
     });
 
-    console.log(JSON.stringify(abi));
-    console.log('Contract deployed at: ', result.options.address);
+    console.log('Contract deployed to: ', result.options.address);
     provider.engine.stop();
 }
 
