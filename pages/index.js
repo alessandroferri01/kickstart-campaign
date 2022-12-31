c76b2d2c6d659673adb0a334dacfe59bce159459
AleFerri01pd <ferri.alessandro2001@gmail.com>
2022-12-31 11:44:55 +0100

    add style and rendering campaigns

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
 import factory from "../ethereum/factory.js";
+import 'semantic-ui-css/semantic.min.css'
 
 class CampaignIndex extends Component {
-    async componentDidMount() {
+    static async getInitialProps() { //methodo globale di NextJs, chiamato prima del rendering del componente. Setta i props globali
         const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-        console.log(campaigns);
+        return {campaigns};
     }
 
+    renderCampaigns() {
+       const items =  this.props.campaigns.map(address => {
+        return {
+            header: address,
+            description: <a>View Campaigns</a>,
+            fluid: true,
+        };
+       });
+
+       return <Card.Group items={items} />
+    } 
+
     render() {
-        return <div>Campaign Index!!</div>
+        return (
+        <div>
+            {this.renderCampaigns()}
+        </div>
+        );
     }
 }
 
