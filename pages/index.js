d522cc2093166ae0fa7de3137440fab3e6a81bdc
AleFerri01pd <ferri.alessandro2001@gmail.com>
2023-01-02 12:37:15 +0100

    added system to get project name

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,43 @@
 import React, { Component } from "react";
 import { Card, Button } from "semantic-ui-react";
 import factory from "../ethereum/factory.js";
+import Campaign from "../ethereum/campaign.js";
 import Layout from "../components/Layout.js";
 
 class CampaignIndex extends Component {
     static async getInitialProps() { //methodo globale di NextJs, chiamato prima del rendering del componente. Setta i props globali
         const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-        return {campaigns};
+        const campaignsNames = await Promise.all(campaigns.map(async (address) => {
+            let campaign = null;
+            let obj = {};
+            campaign = await Campaign(address);
+
+            if(campaign) {
+                obj[address] = await campaign.methods.nameCampaign().call();
+                return obj;
+            }
+        }));
+        
+        return {campaigns, campaignsNames};
     }
 
     renderCampaigns() {
-       const items =  this.props.campaigns.map(address => {
-        return {
-            header: address,
-            description: <a>View Campaigns</a>,
-            fluid: true,
-        };
+        const items = this.props.campaigns.map((address) => {
+            let nameCampaign = this.props.campaignsNames.map((item) => {
+                if(item[address] !== undefined) {
+                    return item[address];
+                }
+            });
+            
+            nameCampaign = nameCampaign.filter(item => item !== undefined);
+
+            return {
+                header: nameCampaign,
+                meta: address.substring(0, 5) + '...' + address.substring(38, 42),
+                description: <a>View Campaigns</a>,
+                fluid: true,
+            };
        });
 
        return <Card.Group items={items} />
