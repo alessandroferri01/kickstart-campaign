976afcca75d77d618b2c2acc63266bfc08128b30
AleFerri01pd <ferri.alessandro2001@gmail.com>
2023-01-03 21:15:11 +0100

    added route to index page

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Card, Button } from "semantic-ui-react";
 import factory from "../ethereum/factory.js";
 import Campaign from "../ethereum/campaign.js";
 import Layout from "../components/Layout.js";
+import { Link } from "../routes";
 
 class CampaignIndex extends Component {
     static async getInitialProps() { //methodo globale di NextJs, chiamato prima del rendering del componente. Setta i props globali
@@ -35,7 +36,11 @@ class CampaignIndex extends Component {
             return {
                 header: nameCampaign,
                 meta: address.substring(0, 5) + '...' + address.substring(38, 42),
-                description: <a>View Campaigns</a>,
+                description: (
+                <Link route={`/campaigns/${address}`}>
+                    <a>View Campaigns</a>
+                </Link>
+                ),
                 fluid: true,
             };
        });
@@ -47,12 +52,15 @@ class CampaignIndex extends Component {
         return (
         <Layout>
             <h3>Open Campaigns</h3>
-            <Button 
-                primary
-                floated="right"
-                content='Create a Campaign' 
-                icon='add circle' 
-                labelPosition='left' />
+            <Link route="/campaigns/new">
+                <Button 
+                    primary
+                    floated="right"
+                    content='Create a Campaign' 
+                    icon='add circle' 
+                    labelPosition='left' />
+            </Link>
+            
 
             {this.renderCampaigns()}
         </Layout>
